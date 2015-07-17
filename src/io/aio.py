0ce6feb6ae0480941779ab4ec90e51e872ded0ba
Philip Li <philip.li@intel.com>
2015-07-17 16:54:53 +0800

    fix Aio.__init__ error
    PIN is not defined
    
    Signed-off-by: Philip Li <philip.li@intel.com>

--- src/io/aio.py
+++ src/io/aio.py
@@ -1,4 +1,4 @@
-from pin import PINS
+from pin import GPIO_PINS
 
 
 class Aio(object):
@@ -6,13 +6,13 @@ class Aio(object):
         if isinstance(arduino_id, int):
             arduino_id = "A%d" % arduino_id
 
-        pin = PINS[arduino_id]
+        pin = GPIO_PINS[arduino_id]
         pin.select()
 
         self.arduino_id = arduino_id
         self.linux_id = pin.linux_id
 
     def value(self):
-        #FIXME consider to open the file once in init, and seek to beginning to read every time
+        # FIXME consider to open the file once in init, and seek to beginning to read every time
         with open("/sys/bus/iio/devices/iio:device0/in_voltage%d_raw" % self.linux_id, 'r') as f:
             return f.readline().rstrip('\r\n')
